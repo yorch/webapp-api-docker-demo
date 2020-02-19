12766e6df6fa33651883054f57c48b312af3514d
Jorge Barnaby <jorge.barnaby@gmail.com>
2020-02-19 06:55:11 -0500

    Fixes

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,4 +3,4 @@
 docker-compose \
   -f docker-compose.build.yml \
   run builder \
-    bash -c "yarn && yarn clean && yarn build api && yarn build app"
+    bash -c "yarn && yarn clean && yarn build api --prod && yarn build app --prod"
