3611e99e367a6ecb608088e4b9e1988b6c398e04
Jorge Barnaby <jorge.barnaby@gmail.com>
2020-02-26 17:28:37 -0500

    Update run-prod to run in detach mode

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# -d argument is to run it in the background as detached, so you can close the SSH session and the Docker stack will keep running
+
 docker-compose \
   -f docker-compose.prod.yml \
   up \
+  -d \
   --build
