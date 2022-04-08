c3bb0cec60e5a60f03cee176209e12877dae32f3
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-04-08 13:56:46 -0400

    Minor fix

--- apps/app/src/app/MovieList.tsx
+++ apps/app/src/app/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { Props } from 'react';
+import React from 'react';
 import { List, Datagrid, TextField, ResourceComponentProps } from 'react-admin';
 
 export const MovieList = (props: ResourceComponentProps) => (
