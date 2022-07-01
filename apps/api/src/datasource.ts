6ddcd664873d268baafb15475a7e8eb36fee7607
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-07-01 00:12:46 -0400

    Replace depracted typeorm methods with new DataSource (#1676)

---
+++ apps/api/src/datasource.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { config } from './config';
+import { Actor, Film } from './entity';
+
+const { database } = config;
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: database.host,
+  port: database.port,
+  username: database.user,
+  password: database.pass,
+  database: database.db,
+  entities: [
+    // `${__dirname}/entity/*.ts`
+    Actor,
+    Film,
+  ],
+  // synchronize: true, // Not to use in production
+  synchronize: false,
+  logging: true,
+});
+
+export const initDataSource = async () =>
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization', err);
+    });
