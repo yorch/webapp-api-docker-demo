b04a49e2f723cd0bbdde9ae50f95cb7fe9894607
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-06-30 23:06:14 -0400

    Unrelated fixes

--- apps/app/src/app/ActorList.tsx
+++ apps/app/src/app/ActorList.tsx
@@ -1,7 +1,7 @@
 import { List, Datagrid, TextField, ResourceComponentProps } from 'react-admin';
 
 export const ActorList = (props: ResourceComponentProps) => (
-  <List {...props} pagination={null}>
+  <List {...props} pagination={false}>
     <Datagrid>
       <TextField source="id" />
       <TextField source="first_name" />
--- apps/app/src/app/MovieList.tsx
+++ apps/app/src/app/MovieList.tsx
@@ -1,7 +1,7 @@
 import { List, Datagrid, TextField, ResourceComponentProps } from 'react-admin';
 
 export const MovieList = (props: ResourceComponentProps) => (
-  <List {...props} pagination={null}>
+  <List {...props} pagination={false}>
     <Datagrid>
       <TextField source="id" />
       <TextField source="title" />
